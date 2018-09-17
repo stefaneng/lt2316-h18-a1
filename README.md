473643ec45106e2728f111becbfb8bb0fcab42e4
Asad Sayeed <asayeed@mbl.ca>
2018-09-18 01:45:31 +0200

    ready to go?

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# lt2316-h18-a1
-LT2316 H18 Assignment 1
+# LT2316 H18 Assignment 1
+
+Git project for implementing assignment 1 in [Asad Sayeed's](https://asayeed.github.io) machine learning class in the University of Gothenburg's Masters
+of Language Technology programme.
+
+The included dataset comes from the [UCI Machine Learning Repository](https://archive.ics.uci.edu/ml/datasets/Balance+Scale).
+
+## Your notes
+
+If you have any notes to include with your submission, put them here.
--- myid3.py
+++ myid3.py
@@ -15,16 +15,22 @@ class DecisionTree:
         # If load_from isn't None, then it should be a file *object*,
         # not necessarily a name. (For example, it has been created with
         # open().)
-        pass
+        print("Initializing classifier.")
+        if load_from is not None:
+            print("Loading from file object.")
 
-    def train(self, X, y, prune=False):
+    def train(self, X, y, attrs, prune=False):
         # Doesn't return anything but rather trains a model via ID3
         # and stores the model result in the instance.
         # X is the training data, y are the corresponding classes the
         # same way "fit" worked on SVC classifier in scikit-learn.
+        # attrs represents the attribute names in columns order in X.
         #
         # Implementing pruning is a bonus question, to be tested by
         # setting prune=True.
+        #
+        # Another bonus question is continuously-valued data. If you try this
+        # you will need to modify predict and test.
         pass
 
     def predict(self, instance):
@@ -37,11 +43,14 @@ class DecisionTree:
         # accuracy, recall, precision, F1-measure, and a confusion matrix.
         # If display=True, print the information to the console.
         # Raise a ValueError if the class is not trained.
-        return {'precision':None,
-                'recall':None,
-                'accuracy':None,
-                'F1':None,
-                'confusion-matrix':None}
+        result = {'precision':None,
+                  'recall':None,
+                  'accuracy':None,
+                  'F1':None,
+                  'confusion-matrix':None}
+        if display:
+            print(result)
+        return result
 
     def __str__(self):
         # Returns a readable string representation of the trained
